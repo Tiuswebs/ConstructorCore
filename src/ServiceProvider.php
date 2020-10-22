4dc6c5b8dc1c15e82aba8bde6d9cf3a027ecb98f
Carlos Escobar <carlosescobar@weblabor.mx>
2020-10-22 14:28:52 -0500

    add push once

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Tiuswebs\ConstructorCore;
 
 use Illuminate\Support\ServiceProvider as Provider;
+use Blade;
 
 class ServiceProvider extends Provider
 {
@@ -14,6 +15,13 @@ class ServiceProvider extends Provider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'constructor');
-        
+
+        Blade::directive('pushonce', function ($expression) {
+		    $isDisplayed = '__pushonce_'.trim(substr($expression, 2, -2));
+		    return "<?php if(!isset(\$__env->{$isDisplayed})): \$__env->{$isDisplayed} = true; \$__env->startPush{$expression}; ?>";
+		});
+		Blade::directive('endpushonce', function ($expression) {
+		    return '<?php $__env->stopPush(); endif; ?>';
+		});
     }
 }
