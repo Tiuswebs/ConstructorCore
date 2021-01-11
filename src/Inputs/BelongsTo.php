c14ed37f859640c1e2caebd92a37ef16ef10ccaa
Carlos Escobar <carlos@xmlteam.com>
2021-01-11 13:13:55 -0600

    Update

--- src/Inputs/BelongsTo.php
+++ src/Inputs/BelongsTo.php
@@ -6,9 +6,17 @@ use Illuminate\Support\Str;
 
 class BelongsTo extends Select
 {
+	public $show_id = true;
+
 	public function setComponent($component)
 	{
 		$options = $component->getBelongsToOptions($this->original_title, $this->column);
 		return $this->options($options->all())->default($options->keys()->random($options->count())->first());
+	}
+
+	public function setShowId($value)
+	{
+		$this->show_id = $value;
+		return $this;
 	}
 }
