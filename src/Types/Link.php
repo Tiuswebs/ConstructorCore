a70fe0ff385c3d0f8882d16af1127d0dd565d165
Carlos Escobar <carlos@xmlteam.com>
2021-04-09 17:52:23 -0500

    Add link

---
+++ src/Types/Link.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tiuswebs\ConstructorCore\Types;
+
+use Tiuswebs\ConstructorCore\Inputs\BackgroundColor;
+use Tiuswebs\ConstructorCore\Inputs\TextColor;
+use Tiuswebs\ConstructorCore\Inputs\Text;
+
+class Link extends Type
+{
+	public function fields() 
+	{
+		return [
+			Text::make($this->title.' Text')->default('Click here'),
+			Text::make($this->title.' Link')->default('#'),
+			TextColor::make($this->title.' Text Color')->default('#fff'),
+			TextColor::make($this->title.' Text Color Hover')->default('#000'),
+		];
+	}
+}
