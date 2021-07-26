e0b60a8a4c8a393d142456ee369658dfd533fbdf
alexkudzul <alexkudzul@gmail.com>
2021-07-26 16:10:00 -0500

    Update Class Pagination

--- src/Pagination.php
+++ src/Pagination.php
@@ -2,31 +2,9 @@
 
 namespace Tiuswebs\ConstructorCore;
 
-use Illuminate\Support\Facades\Cache;
-
 class Pagination extends Core
 {
     public $category = 'pagination';
-    public $page = 0;
-
-    public function load()
-    {
-        parent::load();
-
-        // Save page and size
-        $this->page = request()->page ?? 0;
-        $this->size = Cache::store('array')->get('filter-contents-size', 0);
-
-        // Save number of current items loaded
-        $items = Cache::store('array')->get('filter-contents', []);
-        $items = collect($items)->count();
-        $this->current_items = $items;
-
-        // Dont load component if there is 0 elements with the "Content" option
-        if ($this->size <= 0) {
-            $this->show_view = false;
-        }
-    }
 
     public function getPreviousUrl()
     {
@@ -40,17 +18,6 @@ class Pagination extends Core
 
     public function getUrl($type = '+')
     {
-        if ($type == '+') {
-            $page = $this->page + 1;
-        } else {
-            $page = $this->page - 1;
-        }
-        if ($page < 0) {
-            return;
-        }
-        if ($type == '+' && $this->current_items != $this->size) {
-            return;
-        }
-        return url()->current() . '?page=' . $page . '&size=' . $this->size;
+        return '#';
     }
 }
