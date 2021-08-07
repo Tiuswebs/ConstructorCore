8d206354f02d6beaa0e1609a5a2e9e595614a9bb
Carlos Escobar <carlos@xmlteam.com>
2021-08-07 10:24:16 -0500

    Fix random selection when has filterQuery

--- src/Core.php
+++ src/Core.php
@@ -200,7 +200,8 @@ abstract class Core
     		} else if (isset($this->values->$get) && isset($this->belongs_to_data[$column]) && $this->values->$get!='item') {
     			$result = $this->belongs_to_data[$column]->firstWhere('id', $this->values->$get);	
     		} else if (isset($this->belongs_to_data[$column]) && $this->belongs_to_data[$column]->count() > 0) {
-    			$result = $this->belongs_to_data[$column]->random();
+    			$default_option = collect($item->options)->keys()->random();
+    			$result = $this->belongs_to_data[$column]->firstWhere('id', $default_option);
     		} else if (isset($this->belongs_to_data[$column])) {
     			$this->show_view = false;
     		}
--- src/Inputs/BelongsTo.php
+++ src/Inputs/BelongsTo.php
@@ -9,12 +9,14 @@ class BelongsTo extends Select
 
 	public function getProcessedInput($component)
 	{
-		$options = $component->getBelongsToOptions($this->original_title, $this->column);
-		if(isset($this->filter_query)) {
-			$function = $this->filter_query;
-			$options = $function($options);
-		}
-		return $this->options($options->all())->default('item');
+		return \Cache::store('array')->remember('getProcessedInput:'.$this->column, now()->addHour(), function() use ($component) {
+			$options = $component->getBelongsToOptions($this->original_title, $this->column);
+			if(isset($this->filter_query)) {
+				$function = $this->filter_query;
+				$options = $function($options);
+			}
+			return $this->options($options->all())->default('item');
+		});
 	}
 
 	public function setShowId($value)
