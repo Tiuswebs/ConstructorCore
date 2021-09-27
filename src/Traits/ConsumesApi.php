2cac70726d5359011f6bb299b1c2684bef48d0c5
Carlos Escobar <carlos@xmlteam.com>
2021-09-27 17:18:46 -0500

    Add cache to get Api to speed

--- src/Traits/ConsumesApi.php
+++ src/Traits/ConsumesApi.php
@@ -3,20 +3,25 @@
 namespace Tiuswebs\ConstructorCore\Traits;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 trait ConsumesApi 
 {
 	public function getFromApi($type)
 	{
-    	$url = config('app.tiuswebs_api');
-		$url = "{$url}/api/example_data/{$type}";
-		return collect(json_decode(Http::get($url)->body()))->recursive();
+		return Cache::remember('getFromApi:'.$type, now()->addDay(), function() use ($type) {
+			$url = config('app.tiuswebs_api');
+			$url = "{$url}/api/example_data/{$type}";
+			return collect(json_decode(Http::get($url)->body()))->recursive();
+		});
 	}
 
 	public function dataFromApi($type)
 	{
-    	$url = config('app.tiuswebs_api');
-		$url = "{$url}/api/{$type}";
-		return collect(json_decode(Http::get($url)->body()))->recursive();
+		return Cache::remember('dataFromApi:'.$type, now()->addDay(), function() use ($type) {
+	    	$url = config('app.tiuswebs_api');
+			$url = "{$url}/api/{$type}";
+			return collect(json_decode(Http::get($url)->body()))->recursive();
+		});
 	}
 }
