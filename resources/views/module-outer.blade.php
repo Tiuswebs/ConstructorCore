4234b120bfc50be37addd280e787bdb89d07dc91
Carlos Escobar <carlos@xmlteam.com>
2021-12-31 19:13:41 -0600

    Advances with livewire

--- resources/views/module-outer.blade.php
+++ resources/views/module-outer.blade.php
@@ -2,24 +2,24 @@
 	{!! $value !!}
 @else
 	<section id="{{$core->id}}" class="{{$core->getComponentClass()}}">
-		@if($core->have_background_color && isset($core->values->background_image) && strlen($core->values->background_image) > 0 && strlen($core->have_background_color) > 0)
-			<div style="background-color: {{$core->values->background_color}}; background-image: url({{$core->values->background_image}})" class="{{$core->values->background_classes}}">
-		@elseif($core->have_background_color && isset($core->values->background_image) && strlen($core->values->background_image) > 0)
-			<div style="background-image: url({{$core->values->background_image}})" class="{{$core->values->background_classes}}">
+		@if($core->have_background_color && isset($core->getValues()->background_image) && strlen($core->getValues()->background_image) > 0 && strlen($core->have_background_color) > 0)
+			<div style="background-color: {{$core->getValues()->background_color}}; background-image: url({{$core->getValues()->background_image}})" class="{{$core->getValues()->background_classes}}">
+		@elseif($core->have_background_color && isset($core->getValues()->background_image) && strlen($core->getValues()->background_image) > 0)
+			<div style="background-image: url({{$core->getValues()->background_image}})" class="{{$core->getValues()->background_classes}}">
 		@elseif(strlen($core->have_background_color) > 0)
-			<div style="background-color: {{$core->values->background_color}}" class="{{$core->values->background_classes}}">
+			<div style="background-color: {{$core->getValues()->background_color}}" class="{{$core->getValues()->background_classes}}">
 		@elseif($core->have_background_color)
 			<div>
 		@endif
 		
-		@if($core->have_container && $core->values->with_container)
+		@if($core->have_container && $core->getValues()->with_container)
 		    <div class="container mx-auto">
 		@endif
 		@if($core->have_paddings && $core->have_background_color)
 			<div class="{{$core->getDefaults()['padding_tailwind']}}" style="{{$core->getPaddingStyle()}}">
 		@endif
 		{!! $value !!}
-		@if($core->have_container && $core->values->with_container)
+		@if($core->have_container && $core->getValues()->with_container)
 		    </div>
 		@endif
 		@if($core->have_paddings && $core->have_background_color)
--- src/Builder.php
+++ src/Builder.php
@@ -2,6 +2,8 @@
 
 namespace Tiuswebs\ConstructorCore;
 
+use Livewire\Livewire;
+
 class Builder
 {
 	public $components = [];
@@ -11,6 +13,9 @@ class Builder
 	public function __construct($components)
 	{
 		$this->components = $components;
+		collect($this->components)->each(function($item) {
+			Livewire::component($item->livewire_name, get_class($item));
+		});
 	}
 
 	public function components()
