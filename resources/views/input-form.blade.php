b7d5c6f1727e6f810ebbfe465f69fd2a412f5f19
Luan Himmlisch <luisangelserranocatala@gmail.com>
2022-05-13 00:00:29 -0500

    Added help to form inputs

--- resources/views/input-form.blade.php
+++ resources/views/input-form.blade.php
@@ -1,6 +1,11 @@
-<div class="form-group row">
-    <label class="col-form-label col-sm-3">{{ $input->title }}</label>
-    <div class="col-sm-9">
-    	{!! $input->form() !!}
+<div class="col-sm-{{ $input->bootstrap_width() }}">
+    <div class="form-group row">
+        <label class="col-form-label col-sm-3" title="{{ $input->hover }}">{{ $input->title }}</label>
+        <div class="col-sm-9">
+            {!! $input->form() !!}
+        </div>
+        @if (isset($input->help))
+            <small class="form-text text-muted">{!! $input->help !!}</small>
+        @endif
     </div>
 </div>
