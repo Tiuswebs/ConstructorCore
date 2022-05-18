dc295db2a687bf64ecaa8c8bd233abb8358616ed
Carlos Escobar <skalero01@gmail.com>
2022-05-17 19:14:50 -0500

    add validation

--- resources/views/input-form.blade.php
+++ resources/views/input-form.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-{{ $input->bootstrap_width() }}">
     <div class="form-group row">
-        <label class="col-form-label col-sm-3" title="{{ $input->hover }}">{{ $input->title }}</label>
+        <label class="col-form-label col-sm-3" title="{{ $input->hover ?? '' }}">{{ $input->title }}</label>
         <div class="col-sm-9">
             {!! $input->form() !!}
         </div>
