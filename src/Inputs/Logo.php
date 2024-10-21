15e97081cc6de953ff690ca397ac3cdefdf4ea36
Carlos Escobar <skalero01@gmail.com>
2024-10-21 12:27:49 -0600

    Logo asks for colored or white logo instead of text

--- src/Inputs/Logo.php
+++ src/Inputs/Logo.php
@@ -2,19 +2,28 @@
 
 namespace Tiuswebs\ConstructorCore\Inputs;
 
-class Logo extends Text
+use Illuminate\Support\Str;
+
+class Logo extends Select
 {
-	public $help = 'Will use the default one if empty';
+	public function load()
+	{
+		$this->options = [
+			'photo_url' => __('Main Logo'),
+			'white_logo_url' => __('White Logo'),
+		];
+		$this->default_value = 'photo_url';
+	}
 
 	public function formatValue()
 	{
 		$value = parent::formatValue();
-		if(!is_null($value) && strlen($value)>0) {
-            return $value;
-        }
+		if(!is_null($value) && strlen($value)>0 && Str::startsWith($value, 'http')) {
+			return $value;
+		}
         if(!is_object($this->component->team)) {
         	return;
         }
-        return $this->component->team->photo_url;
+        return $this->component->team->$value;
 	}
 }
