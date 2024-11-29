bd8f49ddb0d91f30eb4db5371d47dbbe4a7541a4
Carlos Escobar <skalero01@gmail.com>
2024-11-28 18:17:23 -0600

    some fixes styles

--- resources/views/input-form.blade.php
+++ resources/views/input-form.blade.php
@@ -1,11 +1,14 @@
-<div class="col-sm-{{ $input->bootstrap_width() }}">
-    <div class="form-group row">
-        <label class="col-form-label col-sm-3" title="{{ $input->hover ?? '' }}">{{ $input->title }}</label>
+<div class="col-sm-{{ $input->bootstrap_width() }} pb-4">
+    <div class="row">
+        <label class="col-sm-3" title="{{ $input->hover ?? '' }}">
+            {{ $input->title }}
+            @if (isset($input->help))
+                <small class="text-gray-400 block text-xs">{!! $input->help !!}</small>
+            @endif
+        </label>
         <div class="col-sm-9">
             {!! $input->form() !!}
         </div>
-        @if (isset($input->help))
-            <small class="form-text text-muted">{!! $input->help !!}</small>
-        @endif
     </div>
+    
 </div>
