369948c4eb6e39a5545e44bdfbaeb7b4d19780e4
Carlos Escobar <skalero01@gmail.com>
2025-04-06 16:26:03 -0600

    add noto sans

--- src/Inputs/FontFamily.php
+++ src/Inputs/FontFamily.php
@@ -37,7 +37,8 @@ class FontFamily extends Select
 			Font::make('Merriweather Sans')->url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300;400;500;600;700;800&display=swap'),
 			Font::make('Open Sans')->url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap'),
 			Font::make('Bai Jamjuree')->url('https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap'),
-			Font::make('Quicksand')->url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap')
+			Font::make('Quicksand')->url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap'),
+			Font::make('Noto Sans')->url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap'),
 		]);
 	}
 }
